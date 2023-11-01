e395a4820e72dde5af8297871800dc61fb8a6cef
cap <798639808@qq.com>
2023-11-01 21:30:45 +0800

    添加阅读进度和发表时间信息，增加EarlyAccess论文

--- IEEE_TEVC.py
+++ IEEE_TEVC.py
@@ -15,16 +15,28 @@ def generate_paper_item(info, save_path):
         print('跳过：', info['Document Title'])
         return 0
     
-    # 处理论文题目中的非法字符: *, :, ?, ", >, <, \, /, |
-    title_processed = re.sub(r"\\[a-zA-Z]+\s*", "", info['Document Title'])
-    title_processed = re.sub(r'[*:?"<>/|]', "_", title_processed)
+    # 获取发表日期：见刊日期（已正式发表），如果没有则选取在线日期（early access）
+    try:
+        publish_date = pd.to_datetime(info['Date Added To Xplore']).strftime("%Y-%m-%d")
+        online_date = pd.to_datetime(info['Online Date']).strftime("%Y-%m-%d")
+        date = publish_date
+    except:
+        publish_date = 'xxx'
+        online_date = pd.to_datetime(info['Online Date']).strftime("%Y-%m-%d")
+        date = f"(EarlyAccess){online_date}"
+
+    # 格式化发表时间
+    date_formalized = f"{online_date}（在线），{publish_date}（见刊）"
+
+    # 格式化论文题目：处理题目中的非法字符: *, :, ?, ", >, <, \, /, |
+    title_formalized = re.sub(r"\\[a-zA-Z]+\s*", "", info['Document Title'])
+    title_formalized = re.sub(r'[*:?"<>/|]', "_", title_formalized)
 
     # 格式化保存文件的路径+名称
-    file_name = save_path.joinpath(f"{info['Publication Year']} - {title_processed}.md")
+    file_name = save_path.joinpath(f"{date} - {title_formalized}.md")
 
     # 格式化论文题目
     num_pages = int(info['End Page']) - int(info['Start Page']) + 1  # 计算全文页数
-    title_formalized = f"{title_processed} ({info['Publication Year']}年) ({num_pages}页)"
 
     # 格式化作者和单位信息
     affiliations_formalized = ''.join([f"*{i+1} - {item}*\n" for i, item in enumerate(info['Author Affiliations'].split("; "))])
@@ -37,45 +49,51 @@ def generate_paper_item(info, save_path):
     # 格式化下载链接
     download_links = f"[期刊官网]({info['PDF Link']}), [SCI-Hub](https://sci-hub.se/{info['DOI']})"
 
+    # 添加阅读进度信息
+    progress = "\n\n- [ ] 已完成"
+
     # 构造写入数据
     data = {
         '论文题目': title_formalized,
+        '发表时间': date_formalized,
         '期刊名': info['Publication Title'],
         '作者': authors_formalized,
         '摘要': info['Abstract'],
         '关键词': keywords_formalized,
         '下载链接': download_links,
+        '阅读进度': progress,
     }
 
     # 写入文件
-    try:
-        with open(file_name, mode='w', encoding='utf8') as f:
-            for key, value in data.items():
-                f.write(f"**{key}**: {value}\n\n")
-        return 1
-
-    except:
-        pass  # debug用
+    with open(file_name, mode='w', encoding='utf8') as f:
+        for key, value in data.items():
+            f.write(f"**{key}**: {value}\n\n")
+            
+    return 1
 
 
 if __name__ == '__main__':
-    save_path = Path('results')
-    save_path.mkdir(exist_ok=True)
+    dirnames = [
+        'meta/IEEE_TEVC_2019_Issue1-6', 
+        'meta/IEEE_TEVC_2020_Issue1-6', 
+        'meta/IEEE_TEVC_2021_Issue1-6', 
+        'meta/IEEE_TEVC_2022_Issue1-6', 
+        'meta/IEEE_TEVC_2023_Issue1-5',
+        'meta/IEEE_TEVC_2023_EarlyAccess20231101'
+    ]
 
-    dirnames = ['meta/IEEE_TEVC_2019_Issue1-6', 
-                'meta/IEEE_TEVC_2020_Issue1-6', 
-                'meta/IEEE_TEVC_2021_Issue1-6', 
-                'meta/IEEE_TEVC_2022_Issue1-6', 
-                'meta/IEEE_TEVC_2023_Issue1-5']
+    save_path = Path('results/TEVC')  # 保存路径
+    save_path.mkdir(parents=True, exist_ok=True)
 
     count = [0, 0]  # 记录文献条目生成数量（跳过数目、成功生成数目）
     for dirname in dirnames:
         for file_path in Path(dirname).iterdir():
             print('读取：', file_path)
 
-            df = pd.read_csv(file_path)
+            df = pd.read_csv(file_path, index_col=False)
             for i in range(len(df)):
                 res = generate_paper_item(df.iloc[i, :], save_path)
                 count[res] += 1
-    print(f'跳过了{count[0]}条非文献信息，成功生成{count[1]}了个文献条目！')
+                
+    print(f'跳过了{count[0]}条非文献信息，成功生成{count[1]}个文献条目！')
 
